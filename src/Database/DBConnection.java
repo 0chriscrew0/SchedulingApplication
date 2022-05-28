c09b7a33323177400678346d03f13ee6f7f98e2f
Christopher Crow <0chriscrew0@gmail.com>
2022-05-28 06:48:05 -0700

    Add separate Password class to hide password

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -1,23 +1,24 @@
 package Database;
 
+import Password.Password;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public abstract class DBConnection {
     private static final String protocol = "jdbc";
     private static final String vendor = ":mysql:";
-    private static final String location = "//localhost/";
+    private static final String location = "//localhost:3306/";
     private static final String databaseName = "client_schedule";
     private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER";
     private static final String driver = "com.mysql.cj.jdbc.Driver";
     private static final String userName = "sqlUser";
-    private static String password = "Passw0rd!";
     private static Connection connection;
 
     public static void openConnection() {
         try {
             Class.forName(driver);
-            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            connection = DriverManager.getConnection(jdbcUrl, userName, Password.getPassword());
             System.out.println("Connection succesful!");
         }
         catch(Exception e) {
---
+++ src/Password/Password.java
@@ -0,0 +1,9 @@
+package Password;
+
+public abstract class Password {
+    private static final String password = "Passw0rd!";
+
+    public static String getPassword() {
+        return password;
+    }
+}
