21249d5e4846754fc235fd41e9d4f49294fbe834
Christopher Crow <0chriscrew0@gmail.com>
2022-05-28 07:45:23 -0700

    Create Country model and DBCountries to get Countries from DB

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 
+import DBAccess.DBCountries;
 import Database.DBConnection;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Country;
 
 public class Main extends Application {
     @Override
@@ -15,14 +18,19 @@ public class Main extends Application {
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
 
-        // open JDBC connection
-        DBConnection.openConnection();
-
-        // close JDBC connection
-        DBConnection.closeConnection();
+        ObservableList<Country> countriesList = DBCountries.getAllCountries();
+        for (Country C : countriesList) {
+            System.out.println("Country ID: " + C.getId() + " Country Name: " + C.getName());
+        }
     }
 
     public static void main(String[] args) {
+        // open JDBC connection
+        DBConnection.openConnection();
+
         launch(args);
+
+        // close JDBC connection
+        DBConnection.closeConnection();
     }
 }
