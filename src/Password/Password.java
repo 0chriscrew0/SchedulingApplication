b6014524e8b2aa6541e5fc2bcaf69d81a455c133
Christopher Crow <0chriscrew0@gmail.com>
2022-05-28 06:59:36 -0700

    Merge remote-tracking branch 'origin/master'

--- src/Password/Password.java
+++
@@ -1,9 +0,0 @@
-package Password;
-
-public abstract class Password {
-    private static final String password = "Passw0rd!";
-
-    public static String getPassword() {
-        return password;
-    }
-}
