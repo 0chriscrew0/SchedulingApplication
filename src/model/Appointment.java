dd9a062b45b5c6a3109677b9efe39c6b52b2d05b
Christopher Crow <0chriscrew0@gmail.com>
2022-06-01 12:04:29 -0700

    Add Appointment model and DBAppointment class

---
+++ src/model/Appointment.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+
+    private int ID;
+    private String title;
+    private String description;
+    private String location;
+    private String type;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private int customerID;
+    private int userID;
+    private int contactID;
+
+    public Appointment(int ID, String title, String description, String location, String type, LocalDateTime start, LocalDateTime end, int customerID, int userID, int contactID) {
+        this.ID = ID;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+        this.customerID = customerID;
+        this.userID = userID;
+        this.contactID = contactID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+}
