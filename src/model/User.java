a5305cf745b6bc78fd3cdaa6f14fb2394156ba94
Christopher Crow <0chriscrew0@gmail.com>
2022-06-01 06:35:30 -0700

    Add User model

---
+++ src/model/User.java
@@ -0,0 +1,21 @@
+package model;
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+
+    public User(int userId, String username) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
