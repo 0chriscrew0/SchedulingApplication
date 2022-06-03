d87a86c0f09a519ec3d7beaa22e007bd7ec6974d
Christopher Crow <0chriscrew0@gmail.com>
2022-06-03 13:47:27 -0700

    Add functionality to Add Customer form

--- src/DBAccess/DBFirstLevelDivision.java
+++ src/DBAccess/DBFirstLevelDivision.java
@@ -23,6 +23,31 @@ public class DBFirstLevelDivision {
         return f;
     }
 
+    public static ObservableList<FirstLevelDivision> getFirstLevelDivisionsByCountry(int countryID) {
+        ObservableList<FirstLevelDivision> firstLevelDivisionList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * FROM first_level_divisions WHERE Country_ID = ?";
+
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setInt(1, countryID);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                int ID = rs.getInt("Division_ID");
+                String division = rs.getString("Division");
+
+                FirstLevelDivision f = new FirstLevelDivision(ID, division, countryID);
+                firstLevelDivisionList.add(f);
+            }
+        } catch(SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return firstLevelDivisionList;
+    }
+
     public static ObservableList<FirstLevelDivision> getAllFirstLevelDivisions() {
         ObservableList<FirstLevelDivision> firstLevelDivisionList = FXCollections.observableArrayList();
 
--- src/controller/AddCustomer.java
+++ src/controller/AddCustomer.java
@@ -1,6 +1,8 @@
 package controller;
 
 import DBAccess.DBCountry;
+import DBAccess.DBCustomer;
+import DBAccess.DBFirstLevelDivision;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,16 +11,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import model.Country;
+import model.FirstLevelDivision;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
@@ -31,7 +32,7 @@ public class AddCustomer implements Initializable {
     public TextField postalCodeField;
     public TextField phoneField;
     public ComboBox<Country> countrySelection;
-    public ComboBox firstLevelDivisionSelection;
+    public ComboBox<FirstLevelDivision> firstLevelDivisionSelection;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -52,12 +53,32 @@ public class AddCustomer implements Initializable {
             }
         }
 
+        class DivisionListCell extends ListCell<FirstLevelDivision> {
+
+            @Override
+            public void updateItem(FirstLevelDivision item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item != null) {
+                    setText(item.getDivision());
+                }
+                else {
+                    setText(null);
+                }
+            }
+        }
+
         countrySelection.setCellFactory(listView -> new CountryListCell());
         countrySelection.setButtonCell(new CountryListCell());
+        countrySelection.getSelectionModel().selectFirst();
+        firstLevelDivisionSelection.setItems(DBFirstLevelDivision.getFirstLevelDivisionsByCountry(countrySelection.getValue().getId()));
+        firstLevelDivisionSelection.getSelectionModel().selectFirst();
+        firstLevelDivisionSelection.setCellFactory(listView -> new DivisionListCell());
+        firstLevelDivisionSelection.setButtonCell(new DivisionListCell());
 
         countrySelection.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue != null && oldValue != newValue) {
-                System.out.println(newValue.getName());
+            if(newValue != null) {
+                firstLevelDivisionSelection.setItems(DBFirstLevelDivision.getFirstLevelDivisionsByCountry(newValue.getId()));
+                firstLevelDivisionSelection.getSelectionModel().selectFirst();
             }
         });
     }
@@ -66,17 +87,73 @@ public class AddCustomer implements Initializable {
         Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
         Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 1000, 630);
-        stage.setTitle("Add Customer");
+        stage.setTitle("Scheduling Application");
         stage.setScene(scene);
         stage.show();
     }
 
-    public void onAddButton(ActionEvent actionEvent) {
+    public void onAddButton(ActionEvent actionEvent) throws SQLException, IOException {
+
         String name = nameField.getText();
+        if(name.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Customer Input Error");
+            alert.setHeaderText("Name Field Invalid");
+            alert.setContentText("Name field should contain a non-empty string");
+
+            alert.showAndWait();
+            return;
+        }
+
         String address = addressField.getText();
+        if(address.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Customer Input Error");
+            alert.setHeaderText("Address Field Invalid");
+            alert.setContentText("Address field should contain a non-empty string");
+
+            alert.showAndWait();
+            return;
+        }
+
         String postalCode = postalCodeField.getText();
+        if(postalCode.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Customer Input Error");
+            alert.setHeaderText("Postal Code Field Invalid");
+            alert.setContentText("Postal code field should contain a non-empty string");
+
+            alert.showAndWait();
+            return;
+        }
+
         String phone = phoneField.getText();
-//        int divisionID = firstLevelDivisionSelection.getSelectionModel();
+        if(phone.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Customer Input Error");
+            alert.setHeaderText("Phone Field Invalid");
+            alert.setContentText("Phone field should contain a non-empty string");
+
+            alert.showAndWait();
+            return;
+        }
+
+        int divisionID = firstLevelDivisionSelection.getValue().getDivisionID();
+
+        int rowsAffected = DBCustomer.insert(name, address, postalCode, phone, divisionID);
+
+        if(rowsAffected != 0) {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
+            Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1000, 630);
+            stage.setTitle("Scheduling Application");
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Insert Failed");
+            errorAlert.setContentText("There was a problem and the customer was not added.\nPlease try again.");
+        }
     }
 
     public void onCountrySelect(ActionEvent actionEvent) {
